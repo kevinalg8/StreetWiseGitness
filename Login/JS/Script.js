ae5ec0e9f355c526c4151326450748930562108f
kevinalg8 <kalondono35@misena.edu.co>
2022-11-02 00:43:54 -0500

    LoginPt1

---
+++ Login/JS/Script.js
@@ -0,0 +1,14 @@
+const login = (Bton1, Bton2) => {
+  Bton1 = document.getElementById('Persona'),
+  Bton2 = document.getElementById('Usuario');
+
+
+  Bton1.addEventListener("click", () => {
+    alert("Bienvenidos")
+  })
+  Bton2.addEventListener("click", () => {
+    alert("Bienvenidos")
+  })
+}
+login();
+//Realizar que al ingresar datos los envie y los almacene
