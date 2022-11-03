0c7c44f041869150642f1c2e36f0b3b9a2ea737b
kevinalg8 <kalondono35@misena.edu.co>
2022-11-02 22:01:22 -0500

    LoginPt3

--- Login/JS/Script.js
+++ Login/JS/Script.js
@@ -1,13 +1,12 @@
 const login = (Bton1, Bton2) => {
-  Bton1 = document.getElementById('Persona'),
-  Bton2 = document.getElementById('Usuario');
+  Bton2 = document.getElementById('Usuario'),
+  Bton1 = document.getElementById('Persona');
 
-
-  Bton1.addEventListener("click", () => {
-    alert("Bienvenidos")
-  })
   Bton2.addEventListener("click", () => {
     alert("Bienvenidos")
+  })
+  Bton1.addEventListener("click", () => {
+    alert("Recuerde Iniciar Sesion")
   })
 }
 login();
